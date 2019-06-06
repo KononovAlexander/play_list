1e0eb931688361f52d0d4194505f1d5b4cd5c3e7
FRHigh <kononov8214@gmail.com>
2019-06-06 23:20:11 +0300

    start

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+
+
+(function(){
+'use strict';
+
+
+ var playList = [
+ {
+  author: "LED ZEPPELIN",
+  song:"STAIRWAY TO HEAVEN"
+ },
+ {
+  author: "QUEEN",
+  song:"BOHEMIAN RHAPSODY"
+ },
+ {
+  author: "LYNYRD SKYNYRD",
+  song:"FREE BIRD"
+ },
+ {
+  author: "DEEP PURPLE",
+  song:"SMOKE ON THE WATER"
+ },
+ {
+  author: "JIMI HENDRIX",
+  song:"ALL ALONG THE WATCHTOWER"
+ },
+ {
+  author: "AC/DC",
+  song:"BACK IN BLACK"
+ },
+ {
+  author: "QUEEN",
+  song:"WE WILL ROCK YOU"
+ },
+ {
+  author: "METALLICA",
+  song:"ENTER SANDMAN"
+ }
+];
+
+function print([]){
+   var numList = document.createElement('ol' );
+
+	for(var i = 0; i < playList.length; i++){
+
+        printObj(playList[i]);
+        var li = document.createElement('li' );
+
+        li.innerHTML = out;
+  
+        numList.appendChild(li);
+        
+	}
+    
+    document.body.appendChild(numList);    
+}
+       var out;
+function printObj(o){
+
+  out = [];
+
+  for(var key in o){
+     out .push( `<em>  ${key}</em>` +  ' : '  + `<span>${o[key] }</span>` ) ;
+      
+
+
+      
+      
+  }
+      // return out;
+
+}
+print(playList);
+
+
+
+})();
